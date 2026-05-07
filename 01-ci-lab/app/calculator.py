d81bada2dafb4d3788ee478f84d73c956f101009
Agata Skorupka <agata.skorupka@gmail.com>
2026-05-07 11:50:30 +0200

    feat: update code to be compatible with AWS Lambda

--- 01-ci-lab/app/calculator.py
+++ 01-ci-lab/app/calculator.py
@@ -5,6 +5,20 @@ def add(a, b):
 def divide(a, b):
     return a / b
 
+def lambda_handler(event, context):
+    a = event["a"]
+    b = event["b"]
 
-def divide2(a, b):
-    return a / b
+    operation = event["operation"]
+
+    if operation == "add":
+        result = add(a, b)
+    elif operation == "divide":
+        result = divide(a, b)
+    else:
+        return {"statusCode": 400, "body": "Unsupported operation"}
+
+    return {
+        "statusCode": 200,
+        "body": result
+    }
